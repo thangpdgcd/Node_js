d3a2ef69d39ddedabf137da2a2fafff566339384
Hoang Anh <anhnthgcd201650@fpt.edu.vn>
2023-11-17 13:36:23 +0700

    Reacjs

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize' //thư viện này tách làm 3 môi trường
 //tạo để kết nối database
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('project_1_nodejs', 'root', null, {
+const sequelize = new Sequelize('project_nodejs', 'root', null, {
   host: 'localhost',
   port: 3306,
   dialect: 'mysql'
